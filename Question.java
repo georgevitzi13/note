165b0169f24251dfab8f430bf4b32a58e158f716
georgevitzi13 <georgevitzi13@hotmail.com>
2025-02-06 01:51:45 +0200

    Question.java

--- Question.java
+++ Question.java
@@ -1,4 +1,5 @@
 package ergasia.trivia;
+
 import java.util.List;
 
 public class Question {
@@ -6,20 +7,26 @@ public class Question {
     private List<String> options;
     private String correctAnswer;
 
-    // Constructor
     public Question(String questionText, List<String> options, String correctAnswer) {
         this.questionText = questionText;
         this.options = options;
         this.correctAnswer = correctAnswer;
     }
 
-    // Getters
-    public String getQuestionText() { return questionText; }
-    public List<String> getOptions() { return options; }
-    public String getCorrectAnswer() { return correctAnswer; }
+    public String getQuestionText() {
+        return questionText;
+    }
 
-    // Check answer
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
     public boolean isCorrect(String userAnswer) {
         return correctAnswer.equalsIgnoreCase(userAnswer);
     }
 }
+
