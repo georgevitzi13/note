afe5c5d6b1190739efe530dbbfa194a8e715738d
georgevitzi13 <georgevitzi13@hotmail.com>
2025-02-06 01:49:47 +0200

    GameSettings.java

--- GameSettings.java
+++ GameSettings.java
@@ -1,21 +1,21 @@
 package ergasia.trivia;
+
 public class GameSettings {
     private String category;
     private String type;
-    private int questionCount;
+    private int amount;
     private String difficulty;
 
-    // Constructor
-    public GameSettings(String category, String type, int questionCount, String difficulty) {
+    public GameSettings(String category, String type, int amount, String difficulty) {
         this.category = category;
         this.type = type;
-        this.questionCount = questionCount;
+        this.amount = amount;
         this.difficulty = difficulty;
     }
 
     // Getters
     public String getCategory() { return category; }
     public String getType() { return type; }
-    public int getQuestionCount() { return questionCount; }
+    public int getAmount() { return amount; }
     public String getDifficulty() { return difficulty; }
 }
